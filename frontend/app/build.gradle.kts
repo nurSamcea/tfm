35fc6347914b242ae2d60c4a70b1eafe624a5ad5
Nuria Alvarez Rio <nuriaalvarezrio@gmail.com>
2025-06-20 18:23:10 +0200

    feat: update skd

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.frontend"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.frontend"
-        minSdk = 24
-        targetSdk = 34
+        minSdk = 21
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
